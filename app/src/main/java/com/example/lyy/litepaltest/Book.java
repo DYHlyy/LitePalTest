7afb6eb1f128bdb601d40a2c31c9c40a89ba0adb
Lyy <420326369@qq.com>
2017-01-07 12:08:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lyy/litepaltest/Book.java
@@ -0,0 +1,69 @@
+package com.example.lyy.litepaltest;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Lyy on 2017/1/7.
+ */
+
+public class Book extends DataSupport{
+    private int id;
+
+    private String author;
+
+    private double price;
+
+    private int pages;
+
+    private String name;
+
+    private String press;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getPress() {
+        return press;
+    }
+
+    public void setPress(String press) {
+        this.press = press;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/example/lyy/litepaltest/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.lyy.litepaltest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.litepal.crud.DataSupport;
+import org.litepal.tablemanager.Connector;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button createDatabase= (Button) findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Connector.getDatabase();
+                Toast.makeText(getApplicationContext(),"Succeed",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button addData= (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Book book=new Book();
+                book.setAuthor("Dan Brown");
+                book.setName("The Da Vinci Code");
+                book.setPages(454);
+                book.setPrice(16.96);
+                book.setPress("Unknow");
+                book.save();
+                Toast.makeText(getApplicationContext(),"Add Success",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button updateData= (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Book book=new Book();
+                book.setPrice(14.95);
+                book.setPress("Anchor");
+                book.updateAll("name=? and author=?","The Lost Symbol","Dan Brown");
+            }
+        });
+
+        Button deleteButton= (Button) findViewById(R.id.delete_data);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DataSupport.deleteAll(Book.class,"price<?","15");
+            }
+        });
+
+        Button queryButton= (Button) findViewById(R.id.query_data);
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List<Book>books=DataSupport.findAll(Book.class);
+                for(Book book:books){
+                    Log.d("MainActivity","book name is"+book.getName());
+                    Log.d("MainActivity","book author is"+book.getAuthor());
+                    Log.d("MainActivity","book pages is"+book.getPages());
+                    Log.d("MainActivity","book price is"+book.getPrice());
+                    Log.d("MainActivity","book press is"+book.getPress());
+
+
+                }
+            }
+        });
+    }
+}
